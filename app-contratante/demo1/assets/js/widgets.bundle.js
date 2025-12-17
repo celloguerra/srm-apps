2c3d37034202aa2428dcaba7b427df523cafe1e8
CG <celloguerra@gmail.com>
2025-12-17 20:02:06 -0300

    ajustes

--- app-contratante/demo1/assets/js/widgets.bundle.js
+++ app-contratante/demo1/assets/js/widgets.bundle.js
@@ -1436,52 +1436,6 @@ KTUtil.onDOMContentLoaded(function () {
 "use strict";
 
 // Class definition
-var KTTimelineWidget24 = function () {
-    // Private methods
-    var handleActions = function() {
-        var card = document.querySelector('#kt_list_widget_24');        
-        
-        if ( !card ) {
-            return;
-        }
-
-        // Checkbox Handler
-        KTUtil.on(card, '[data-kt-element="follow"]', 'click', function (e) {
-            if ( this.innerText === 'Following' ) {
-                this.innerText = 'Follow';
-                this.classList.add('btn-light-primary');
-                this.classList.remove('btn-primary');
-                this.blur();
-            } else {
-                this.innerText = 'Following';
-                this.classList.add('btn-primary');
-                this.classList.remove('btn-light-primary');
-                this.blur();
-            }
-        });
-    }
-
-    // Public methods
-    return {
-        init: function () {           
-            handleActions();             
-        }   
-    }
-}();
-
-// Webpack support
-if (typeof module !== 'undefined') {
-    module.exports = KTTimelineWidget24;
-}
-
-// On document ready
-KTUtil.onDOMContentLoaded(function() {
-    KTTimelineWidget24.init();
-}); 
-
-"use strict";
-
-// Class definition
 var KTChartsWidget1 = function () {
     var chart = {
         self: null,
@@ -16076,6 +16030,52 @@ KTUtil.onDOMContentLoaded(function() {
 
 
  
+"use strict";
+
+// Class definition
+var KTTimelineWidget24 = function () {
+    // Private methods
+    var handleActions = function() {
+        var card = document.querySelector('#kt_list_widget_24');        
+        
+        if ( !card ) {
+            return;
+        }
+
+        // Checkbox Handler
+        KTUtil.on(card, '[data-kt-element="follow"]', 'click', function (e) {
+            if ( this.innerText === 'Following' ) {
+                this.innerText = 'Follow';
+                this.classList.add('btn-light-primary');
+                this.classList.remove('btn-primary');
+                this.blur();
+            } else {
+                this.innerText = 'Following';
+                this.classList.add('btn-primary');
+                this.classList.remove('btn-light-primary');
+                this.blur();
+            }
+        });
+    }
+
+    // Public methods
+    return {
+        init: function () {           
+            handleActions();             
+        }   
+    }
+}();
+
+// Webpack support
+if (typeof module !== 'undefined') {
+    module.exports = KTTimelineWidget24;
+}
+
+// On document ready
+KTUtil.onDOMContentLoaded(function() {
+    KTTimelineWidget24.init();
+}); 
+
 "use strict";
 
 // Class definition
